7303e9f351cac6cad9801e4649d668107aeaaf9e
Josh Samuelson <josh@joshsamuelson.com>
2022-09-06 21:50:00 -0700

    03_04 solutions

--- variables.tf
+++ variables.tf
@@ -1,10 +1,4 @@
-#variable "instance_type" {
-#  description = "Type of EC2 instance to provision"
-#  default     = "t3.nano"
-#}
-
-
-variable "subnet_prv1" {
-  description = "Private Subnet 1"
-  default = "subnet-0f4c5cc3e9b9b4dac"
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t3.nano"
 }
