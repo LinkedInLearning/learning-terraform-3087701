60743904246da74d6502dcfddee3dd49e0734487
Matt Black <mblackonline.git@gmail.com>
2022-11-18 11:31:40 -0500

    corrected typo

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_groups_arns  = module.blog_alb.target_groups_arns
+  target_group_arns  = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
   
   image_id               = data.aws_ami.app_ami.id
