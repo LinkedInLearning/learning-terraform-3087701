8f3847c019b54be311dce7c53a93ced70aac16b5
Matt Black <mblackonline.git@gmail.com>
2022-11-17 21:34:01 -0500

    updated aws_instance name to 'blog'

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
