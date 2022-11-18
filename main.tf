f90e56a33f918925661551fb6971705d1caec95e
Matt Black <mblackonline.git@gmail.com>
2022-11-18 11:27:28 -0500

    autoscaling

--- main.tf
+++ main.tf
@@ -31,18 +31,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.3"
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+  name     = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_groups_arns  = module.blog_alb.target_groups_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  
+  image_id               = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
