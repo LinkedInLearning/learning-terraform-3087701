84ebcad489f970ef2b4882daa587b9466d14795c
Matt Black <mblackonline.git@gmail.com>
2022-11-18 11:30:07 -0500

    remove alb target

--- main.tf
+++ main.tf
@@ -65,12 +65,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   ]
 
