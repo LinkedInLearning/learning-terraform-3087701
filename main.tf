4a20f1c93e4e597a9f0428ab5775663091ba7124
Matt Black <mblackonline.git@gmail.com>
2022-11-18 10:31:18 -0500

    Created dev vpc.

--- main.tf
+++ main.tf
@@ -14,9 +14,23 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
+
 
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
@@ -33,7 +47,7 @@ module "blog_sg" {
   version = "4.16.2"
   name    = "blog"
 
-  vpc_id  = data.aws_vpc.default.id
+  vpc_id  = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
