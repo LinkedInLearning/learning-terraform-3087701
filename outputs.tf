3f00b92b8330ef1ff91b3521a30fc4626bf07d36
Matt Black <mblackonline.git@gmail.com>
2022-11-17 21:55:10 -0500

    made updates to providers.tf  and main.tf to match lessons.

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ami" {
-  value = aws_instance.web.ami
+  value = aws_instance.blog.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.web.arn
+  value = aws_instance.blog.arn
 }
