fb4e62f34cd3eff6a64f31eb0430741781aba8a4
MAYAPERETZ <maya.peretz2@gmail.com>
2022-11-27 14:32:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = "a1.medium"
   availability_zone = "us-east-2b"
 
   tags = {
