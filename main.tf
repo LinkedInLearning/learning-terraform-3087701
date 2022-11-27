41b044b983d34073329a89c16094543f72971021
MAYAPERETZ <maya.peretz2@gmail.com>
2022-11-27 15:01:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "a1.medium"
+  instance_type = "t3.nano"
 
   tags = {
     Name = "HelloWorld"
