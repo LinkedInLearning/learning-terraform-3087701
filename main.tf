93e6fe79afc14c4af2a0feb066abf4ee7cb749df
ashokrk7 <119532722+ashokrk7@users.noreply.github.com>
2022-12-06 23:54:43 +0000

    sg

--- main.tf
+++ main.tf
@@ -22,46 +22,23 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
 
-resource "aws_security_group" "blog" {
-  name         = "my-sg"
-  description = "Allow HTTP AND HTTPS IN test_webserver"
-
-  vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "outbound_rule" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
+  vpc_id  = data.aws_vpc.default.id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
