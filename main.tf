200bb07b37783cd5152a0d42f898cdba16557ec5
andrey613 <andrey6131@gmail.com>
2022-12-08 16:15:55 +0200

    fix

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_groups_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "HelloWorld"
@@ -43,7 +43,7 @@ resource "aws_security_group_rule" "blog_http_in"{
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  seecurity_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https_in"{
@@ -53,7 +53,7 @@ resource "aws_security_group_rule" "blog_https_in"{
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  seecurity_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
@@ -62,5 +62,6 @@ resource "aws_security_group_rule" "blog_everything_out" {
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
+  
   security_group_id = aws_security_group.blog.id
 }
