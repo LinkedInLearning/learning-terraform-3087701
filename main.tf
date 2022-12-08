002ffb2a991cc93c45d329ee155f0cb44d76e650
andrey613 <andrey6131@gmail.com>
2022-12-08 16:09:33 +0200

    fix

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "blog"{
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group" "blog_http_in"{
+resource "aws_security_group_rule" "blog_http_in"{
   type        = "ingress"
   from_port   = 80
   to_port     = 80
@@ -46,7 +46,7 @@ resource "aws_security_group" "blog_http_in"{
   seecurity_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group" "blog_https_in"{
+resource "aws_security_group_rule" "blog_https_in"{
   type        = "ingress"
   from_port   = 443
   to_port     = 443
