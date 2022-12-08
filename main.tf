6a45c788dcbdd5103463c52ccb1b3ada21a692ee
andrey613 <andrey6131@gmail.com>
2022-12-08 16:21:38 +0200

    asd

--- main.tf
+++ main.tf
@@ -56,12 +56,13 @@ resource "aws_security_group_rule" "blog_https_in"{
   security_group_id = aws_security_group.blog.id
 }
 
+
 resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
-  
+
   security_group_id = aws_security_group.blog.id
 }
