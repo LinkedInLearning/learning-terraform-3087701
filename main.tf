27f44e0f4722910ab61070737041a879bc032a89
mperetz <mperetz@redhat.com>
2022-12-16 09:07:33 +0200

    Adding network interface and subnet

--- main.tf
+++ main.tf
@@ -26,10 +26,34 @@ resource "aws_vpc" "prod-vpc" {
     }
 }
 
+resource "aws_subnet" "prod-subnet" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "prod-interface" {
+  subnet_id   = aws_subnet.prod-subnet.id
+  private_ips = ["10.0.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.blog.id]
+  
+  network_interface {
+    network_interface_id = aws_network_interface.prod-interface.id
+    device_index         = 0
+  }
 
   tags = {
     Name = "blog-tf"
