1ac0346ac9f56dc5c56cde4d4debab6ed3687969
Ubuntu <ubuntu@ip-172-31-27-213.ap-southeast-1.compute.internal>
2022-12-17 19:54:10 +0000

    fix error

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "blog" {
+resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
