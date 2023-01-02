a6b75d53e0dadc3b35692eb0cfd192a5e42ccd18
Jeri G <108591211+jgreenwi@users.noreply.github.com>
2023-01-02 14:01:35 -0600

    create dev vpc

--- main.tf
+++ main.tf
@@ -15,8 +15,22 @@ data "aws_ami" "app_ami" {
 }
 
 
-data "aws_vpc" "default" {
-  default = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+ 
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
@@ -35,7 +49,7 @@ module "security-group" {
   version = "4.16.2"
   name    = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets
   
   
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
