bc101b5efac27640587434646dd5ae880ce56d8f
Jorge Garcia <jorgarcia1994@gmail.com>
2023-01-03 08:42:30 -0500

    uncomment variables, outputs, and main

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-#output "instance_ami" {
-#  value = aws_instance.web.ami
-#}
+output "instance_ami" {
+ value = aws_instance.web.ami
+}
 
-#output "instance_arn" {
-#  value = aws_instance.web.arn
-#}
+output "instance_arn" {
+ value = aws_instance.web.arn
+}
