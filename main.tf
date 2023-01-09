8cf17086493c1e29655c9703c045ee4395604aee
masterleezy <43220869+masterleezy@users.noreply.github.com>
2023-01-08 22:36:13 -0800

    Use modeule for security group

--- main.tf
+++ main.tf
@@ -19,23 +19,63 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+  
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "Learning Terraform"
+    Name = "HelloWorld"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "4.16.2"
+  name = "blog_new"
 
-  vpc_id  = data.aws_vpc.default.id
-  name    = "blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+
+  engress_rules       = ["all-all"]
+  engress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group" "blog" {
+  name        = "blog"
+  description = "Allow http and https in. Allow everything out"
+  
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  
+  security_group_id = aws_security_group.blog.id
 }
