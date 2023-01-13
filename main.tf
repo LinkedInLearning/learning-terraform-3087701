5efbc08e9eb3e3b6056231bc0114a8a419532bd7
mihiranv <122337874+mihiranv@users.noreply.github.com>
2023-01-13 15:54:37 -0500

    removing error

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in.Allow everything out"
 
-  vpc_id.     = data.aws_vpc.default.id
+  vpc_id      = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
