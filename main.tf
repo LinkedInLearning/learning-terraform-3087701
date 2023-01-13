521856ffc28a4b5c80f241732e6523db65cc16a9
mihiranv <122337874+mihiranv@users.noreply.github.com>
2023-01-13 15:57:02 -0500

    correcting another typo

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "learning Terraform"
