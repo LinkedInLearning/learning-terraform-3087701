e3882374930558c5d4426672fbd7b0b7216d922d
mihiranv <122337874+mihiranv@users.noreply.github.com>
2023-01-13 15:36:43 -0500

    adding blog

--- main.tf
+++ main.tf
@@ -14,11 +14,12 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   tags = {
-    Name = "HelloWorld"
+    Name = "learning Terraform"
   }
 }
+
