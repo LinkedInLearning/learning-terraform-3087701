c8bafe112234be42ab9ae6a04793b8a64ecca57c
mihiranv <122337874+mihiranv@users.noreply.github.com>
2023-01-13 15:55:54 -0500

    adding double quotes around cidr blocks

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  cidr_blocks       = [0.0.0.0/0]
+  cidr_blocks       = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
@@ -48,7 +48,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = [0.0.0.0/0]
+  cidr_blocks       = ["0.0.0.0/0"]
   
   security_group_id = aws_security_group.blog.id
 }
@@ -58,7 +58,7 @@ resource "aws_security_group_rule" "blog_everything_out" {
   from_port         = 0
   to_port           = 0
   protocol         = "-1"
-  cidr_blocks       = [0.0.0.0/0]
+  cidr_blocks       = ["0.0.0.0/0"]
   
   security_group_id = aws_security_group.blog.id
 }
