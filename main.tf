990bf3d943776681ae527406e16a6222839fcbb8
mihiranv <122337874+mihiranv@users.noreply.github.com>
2023-01-14 13:35:43 -0500

    correcting the code

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ data "aws_ami" "app_ami" {
 
   owners = ["979382823631"] # Bitnami
 }
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -31,13 +31,13 @@ module "vpc" {
   }
 }
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "learning Terraform"
+    Name = "Learning Terraform"
   }
 }
 
@@ -49,8 +49,8 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.vpc.vpc_id
-  subnets            = module.vpc.public_subnets
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
@@ -87,7 +87,7 @@ module "blog_sg" {
   
   name = "blog"
 
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
