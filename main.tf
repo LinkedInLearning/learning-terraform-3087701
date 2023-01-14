77aa2369ec7ac32075bd617dc5bc12987bbf3fb4
mihiranv <122337874+mihiranv@users.noreply.github.com>
2023-01-14 13:20:33 -0500

    corrected original author error

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ module "blog_sg" {
   
   name = "blog"
 
-  vpc_id      = module.vpc.public_subnets[0]
+  vpc_id      = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
