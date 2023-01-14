1e826dfc7876fe567f7b6eb0a2111fcd98a6dbb8
mihiranv <122337874+mihiranv@users.noreply.github.com>
2023-01-14 13:13:40 -0500

    trying to fix module name

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ data "aws_ami" "app_ami" {
 
   owners = ["979382823631"] # Bitnami
 }
-module "blog_vpc" {
+module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -49,8 +49,8 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
@@ -87,7 +87,7 @@ module "blog_sg" {
   
   name = "blog"
 
-  vpc_id      = module.blog_vpc.public_subnets[0]
+  vpc_id      = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
