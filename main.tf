80d13793c79d1f5a927bef4a522848e776337102
mihiranv <122337874+mihiranv@users.noreply.github.com>
2023-01-14 13:37:28 -0500

    extra code removal

--- main.tf
+++ main.tf
@@ -23,7 +23,6 @@ module "blog_vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
-  enable_dns_hostnames = true
 
   tags = {
     Terraform = "true"
