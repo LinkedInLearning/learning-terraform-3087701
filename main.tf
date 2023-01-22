bfbcd452cbe302734adb119cd789a6fb583044e9
Adebanjo Adeniyi <nextbrand16@gmail.com>
2023-01-22 15:57:44 +0000

    Revert to original main

--- main.tf
+++ main.tf
@@ -13,11 +13,12 @@ data "aws_ami" "app_ami" {
 
   owners = ["979382823631"] # Bitnami
 }
-resource "aws_instance" "blog" {
+
+resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  instance_type = "t3.nano"
 
   tags = {
-    Name = "Learning Terraform"
+    Name = "HelloWorld"
   }
 }
