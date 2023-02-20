9d0b886e1adcddf7d3850faa30efbc2bb6e5c80f
antanasproductM <98045553+antanasproductM@users.noreply.github.com>
2023-02-20 15:23:08 +0100

    Create Security Group

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "blog" {
 }
 
 resource "aws_security_group_rule" "blog_http_in {
-  type = "ingress"
+  type        = "ingress"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
