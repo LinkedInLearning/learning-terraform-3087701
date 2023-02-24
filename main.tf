f6f110bd079606f4ee3bfbe9fd5168c5276d39f1
Eric T <ektroup@outlook.com>
2023-02-23 21:39:05 -0800

    commit

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
