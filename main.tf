bd8dc75e3820be757009def62184c159f896a37b
antanasproductM <98045553+antanasproductM@users.noreply.github.com>
2023-03-02 11:38:08 +0100

    Create Security group

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ resource "aws_instance" "blog" {
 
 resource "aws_security_group" "blog" {
   name = "blog"
+  description = "Allow http and https in. Allow everything out"
   tags = {
     Terraform = "true"
   }
