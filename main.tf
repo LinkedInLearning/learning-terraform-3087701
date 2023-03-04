19eccaa52c0c56d03313a288168e6b3ec2c7a411
darshanaganguly <80516162+darshanaganguly@users.noreply.github.com>
2023-03-04 15:21:07 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   
-  vpc_security_groups_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "HelloWorld"
