01d6466943263f6b87f4b815953864be69cfe114
darshanaganguly <80516162+darshanaganguly@users.noreply.github.com>
2023-03-04 16:37:29 +0000

    Load Balancer

--- main.tf
+++ main.tf
@@ -42,6 +42,61 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+  
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "my-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = module.blog_vpc.security_group_id
+
+#   access_logs = {
+#     bucket = "my-alb-logs"
+#   }
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = "aws_instance.blog.id"
+          port = 80
+        }
+      }
+   }
+  ]
+
+#   https_listeners = [
+#     {
+#       port               = 443
+#       protocol           = "HTTPS"
+#       certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+#       target_group_index = 0
+#     }
+#   ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+    
+      
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
