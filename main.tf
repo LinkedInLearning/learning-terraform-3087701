5cd9a1d870d6322bf6856fa05f43120f7cb007f3
darshanaganguly <80516162+darshanaganguly@users.noreply.github.com>
2023-03-04 16:41:34 +0000

    LB

--- main.tf
+++ main.tf
@@ -51,8 +51,8 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
 #   access_logs = {
