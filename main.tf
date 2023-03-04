d3d963a2190dfa2bdc305cd1b12400b6fc558ba2
darshanaganguly <80516162+darshanaganguly@users.noreply.github.com>
2023-03-04 15:20:07 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port = 80
   to_port = 80
   protocol = "tcp"
-  cide_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
   
   security_group_id = aws_security_group.blog.id
 }
@@ -50,7 +50,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port = 443
   to_port = 443
   protocol  ="tcp"
-  cide_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
   
   security_group_id = aws_security_group.blog.id
 }
@@ -61,7 +61,7 @@ resource "aws_security_group_rule" "blog_everything_out" {
   from_port = 0
   to_port = 0
   protocol  = "-1"
-  cide_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
   
   security_group_id = aws_security_group.blog.id
 }
