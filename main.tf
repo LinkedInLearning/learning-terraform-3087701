4ff9466517fe1d21e35b067e0e22f07368312256
darshanaganguly <80516162+darshanaganguly@users.noreply.github.com>
2023-03-04 15:58:59 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,10 +36,10 @@ module "blog_sg" {
   
   vpc_id = data.aws_vpc.default.id
   
-  ingress_rules = [http-80-tcp, https-8443-tcp]
+  ingress_rules = ["http-80-tcp", "https-8443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   
-  egress_rules = [all-all]
+  egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
   
