2d7a2a00dcb19f671db69f8ba466d28c9d0e7f9f
antanasproductM <98045553+antanasproductM@users.noreply.github.com>
2023-03-10 17:16:49 +0100

    Refactor to use vars

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,23 +11,23 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner] 
 }
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "$(var.environment.network_prefix).0.0/16"
 
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -36,10 +36,10 @@ module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
-  name = "blog"
+  name = "${var.environment.name}-blog"
 
-  min_size            = 1
-  max_size            = 2
+  min_size            = var.min_size
+  max_size            = var.max_size
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
@@ -51,7 +51,7 @@ module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "blog-alb"
+  name = "${var.environment.name}-blog-alb"
 
   load_balancer_type = "application"
 
@@ -61,7 +61,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.environment.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -77,7 +77,7 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -86,7 +86,7 @@ module "blog_sg" {
   version = "4.13.0"
 
   vpc_id  = module.blog_vpc.vpc_id
-  name    = "blog"
+  name    = "${var.environment.name}-blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
--- variables.tf
+++ variables.tf
@@ -2,3 +2,40 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter"{
+  description = "Name filter and owner for AMI"
+
+  type = object({
+    name  = string
+    owner = string
+  })
+
+  default {
+    name   = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owners = ["979382823631"] # Bitnami
+  }
+}
+
+variable "environment" {
+  description = "Developement Environment"
+
+  type = object ({
+    name            = string
+    network_prefix  = string 
+  })
+
+  default = {
+    name = "dev"
+    cidr = "10.0"
+  }
+}
+
+variable "asg_min_size" {
+  description = "Minumum number of instances in the ASG"
+  default = 1
+}
+variable "asg_max_size" {
+  description = "Maximum number of instances in the ASG"
+  default = 2
+}
