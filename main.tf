6b7864f6e31328f74d72b7cd794ebd9c19031278
antanasproductM <98045553+antanasproductM@users.noreply.github.com>
2023-03-10 15:12:36 +0100

    error fix

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ module "blog_sg" {
   version = "4.17.1"
   name = "blog"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
