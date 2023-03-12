6fdd49acb1a4e0c2c5a8ccbd90badf857b67a877
pr0xxy <james_notoma@hotmail.com>
2023-03-12 17:39:47 -0400

    Create Security Group part 2

--- main.tf
+++ main.tf
@@ -30,11 +30,10 @@ resource "aws_instance" "blog" {
 }
 
 
-
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
-  name    = "blog_new"
+  name    = "blog"
 
   vpc_id              = data.aws_vpc.default.id
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
@@ -44,42 +43,3 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow http and https in. Allow everything out"
-  
-  vpc_id = data.aws_vpc.default.id
-
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
