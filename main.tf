9d9b872ef9dac6cbbe23be4de38a7c29b1002f32
pr0xxy <james_notoma@hotmail.com>
2023-03-12 16:53:56 -0400

    Create security Group

--- main.tf
+++ main.tf
@@ -37,31 +37,31 @@ resource "aws_security_group" "blog" {
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
-  type      = "ingress"
-  from_port = 80
-  to_port   = 80
-  protocol  = "tcp"
-  cidr_block ["0.0.0.0/0"]
+  type       = "ingress"
+  from_port  = 80
+  to_port    = 80
+  protocol   = "tcp"
+  cidr_block = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
-  type      = "ingress"
-  from_port = 443
-  to_port   = 443
-  protocol  = "tcp"
-  cidr_block ["0.0.0.0/0"]
+  type       = "ingress"
+  from_port  = 443
+  to_port    = 443
+  protocol   = "tcp"
+  cidr_block = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
-  type      = "egress"
-  from_port = 0
-  to_port   = 0
-  protocol  = "-1"
-  cidr_block ["0.0.0.0/0"]
+  type       = "egress"
+  from_port  = 0
+  to_port    = 0
+  protocol   = "-1"
+  cidr_block = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
