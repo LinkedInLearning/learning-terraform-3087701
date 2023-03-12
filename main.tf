2d3b6015126d046cc5900707e7addefc08aeaeba
pr0xxy <james_notoma@hotmail.com>
2023-03-12 18:58:09 -0400

    blog

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "alb" {
   load_balancer_type = "application"
 
   vpc_id             = module.vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
+  subnets            = module.vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
