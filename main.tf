2f51a74106bbeba899100ccefb8a77c20ae6bb43
pr0xxy <james_notoma@hotmail.com>
2023-03-12 17:29:36 -0400

    Use Module for Security Group

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "blog" {
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
 }
 
@@ -37,7 +37,7 @@ module "blog_sg" {
   name    = "blog_new"
 
   vpc_id              = data.aws_vpc.default.id
-  ingress_rules       = ["http-80-tcp, "https-443-tcp"]
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules       = ["all-all"]
@@ -55,30 +55,30 @@ resource "aws_security_group" "blog" {
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
-  type       = "ingress"
-  from_port  = 80
-  to_port    = 80
-  protocol   = "tcp"
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
-  type       = "ingress"
-  from_port  = 443
-  to_port    = 443
-  protocol   = "tcp"
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
-  type       = "egress"
-  from_port  = 0
-  to_port    = 0
-  protocol   = "-1"
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
