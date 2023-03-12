30c3eac3b8ae1a6729597355515d0123922be07b
pr0xxy <james_notoma@hotmail.com>
2023-03-12 19:03:11 -0400

    error

--- main.tf
+++ main.tf
@@ -34,7 +34,6 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
- 
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
