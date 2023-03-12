0702e168f90a61f6cefc57076422e424b23d85b5
pr0xxy <james_notoma@hotmail.com>
2023-03-12 18:04:25 -0400

    array for sg

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "alb" {
 
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
-  security_groups    = module.blog_sg.security_group_id
+  security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
