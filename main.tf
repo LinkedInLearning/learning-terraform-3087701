c18c7acdd3f30a838a5d88aabfb1b333059c1878
pr0xxy <james_notoma@hotmail.com>
2023-03-12 16:52:08 -0400

    create security group

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "blog" {
 
 resource "aws_security_group" "blog" {
   name        = "blog"
-  description = "Allow http and https in. Allow everything out".
+  description = "Allow http and https in. Allow everything out"
   
   vpc_id = data.aws_vpc.default.id
 }
