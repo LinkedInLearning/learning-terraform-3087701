f10ed14d21319b94b76047ab080c6ca926352977
antanasproductM <98045553+antanasproductM@users.noreply.github.com>
2023-03-13 09:15:56 +0100

    Define QA env

---
+++ qa/main.tf
@@ -0,0 +1,11 @@
+module "qa" {
+    source = "../modules/blog"
+
+    environment = {
+        name           = "qa"
+        network_prefix = "10.1"
+    }
+
+    asg_min_size = 1
+    asg_max_size = 1
+}
