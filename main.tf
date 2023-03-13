1785f9ac91f2f9cd7435ce0b4b1555fbc5633792
pr0xxy <james_notoma@hotmail.com>
2023-03-12 21:07:34 -0400

    blob_vpc

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ module "blog_sg" {
   version = "4.17.1"
   name    = "blog"
 
-  vpc_id = module.vpc.vpc_id 
+  vpc_id = module.blob_vpc.vpc_id 
   
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
