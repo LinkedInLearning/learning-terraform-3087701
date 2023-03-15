2bb1e1d1e1a49dcdca8fa2e3c0bc8b0542fba4e0
m-fernandezirungaray <126617634+m-fernandezirungaray@users.noreply.github.com>
2023-03-15 14:22:11 -0300

    changing region for east

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  azs             = ["us-east-1a","us-east-1b","us-east-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
