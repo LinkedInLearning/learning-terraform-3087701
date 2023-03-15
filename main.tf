bb6922b685689383331716229d86a9b29ae9be5c
m-fernandezirungaray <126617634+m-fernandezirungaray@users.noreply.github.com>
2023-03-15 12:07:36 -0300

    fixing

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
