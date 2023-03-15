da312599428ab25eaa6b1625a2dba11eaf3b6088
m-fernandezirungaray <126617634+m-fernandezirungaray@users.noreply.github.com>
2023-03-15 13:48:27 -0300

    .

--- main.tf
+++ main.tf
@@ -65,12 +65,6 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws.instance.blog.id
-          port = 80
-        }
-      }
     }
   ]
 
