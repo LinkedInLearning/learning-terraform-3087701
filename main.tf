85ef5f2db2368e873f6b303badc9a6e5a6bf8c83
Oscar Callisaya <oscar.callisaya@nice.com>
2023-04-14 15:09:49 -0400

    Moved all to main

--- main.tf
+++ main.tf
@@ -1,24 +1,11 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
-
-  tags = {
-    Name = "HelloWorld"
-  }
+provider "aws" {
+  region  = "us-west-2"
 }
