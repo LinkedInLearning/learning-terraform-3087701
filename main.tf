7e4749e014f93d4c698b4b527cd31331542025f7
atticus-trellix <117859119+atticus-trellix@users.noreply.github.com>
2023-05-22 13:28:36 -0700

    initial

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = "var.instance_type"
 
   tags = {
     Name = "HelloWorld"
