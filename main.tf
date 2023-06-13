560dbabc228ded937df0af3b3d0930f55635fc70
Nati Biton <natibiton@gmail.com>
2023-06-13 15:24:21 +0300

    Using the registry module

--- main.tf
+++ main.tf
@@ -22,12 +22,27 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
+
+module "blog_sg" { # From the registery of Terraform
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id  # Reference to the above definition of the aws_vpc
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+# Below is the regular definition, currently not used
 
 resource "aws_security_group" "blog" {
   name        = "blog"
