92a1d758bc0db03c00139c46490d98546b9f52e9
Mitak3na <139495390+Mitak3na@users.noreply.github.com>
2023-07-14 14:42:50 +0300

    typo

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
 
-  vpc_id = data.aws_vpc.defalt.id
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
