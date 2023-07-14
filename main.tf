06cccfa4590303561d30100c7bf7ad18b00e45d9
Mitak3na <139495390+Mitak3na@users.noreply.github.com>
2023-07-14 15:23:38 +0300

    use module for SG - typos

--- main.tf
+++ main.tf
@@ -30,16 +30,16 @@ resource "aws_instance" "blog" {
 }
 
 module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
-  name    = "blog_new"
+  source   = "terraform-aws-modules/security-group/aws"
+  version  = "5.1.0"
+  name     = "blog_new"
 
   vpc_id = data.aws_vpc.default.id
 
-  ingress_rules = ["http-80-tcp",https-443-tcp"]
+  ingress_rules       = ["http-80-tcp",https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
