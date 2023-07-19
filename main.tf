7925ec42f78444e22e0db5e9d0a7dba549f163cb
DMVTCSTU <42882087+bamchaar@users.noreply.github.com>
2023-07-18 21:19:08 -0400

    vpc

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ module "blog_sg" {
   version = "5.1.0"
 
   name    = "blog"
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
