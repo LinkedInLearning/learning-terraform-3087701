5a5dd1c66224f35fef60f82c196fc06359990f63
DMVTCSTU <42882087+bamchaar@users.noreply.github.com>
2023-07-18 21:33:32 -0400

    enable_nat_gateway = true

--- main.tf
+++ main.tf
@@ -23,6 +23,8 @@ module "blog_vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
+  enable_nat_gateway = true
+
   tags = {
     Terraform = "true"
     Environment = "dev"
