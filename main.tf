8ca13545e5d6c54518d7705c08c83f2bf36969db
DMVTCSTU <42882087+bamchaar@users.noreply.github.com>
2023-07-18 21:55:53 -0400

    fix

--- main.tf
+++ main.tf
@@ -59,23 +59,6 @@ module "blog_alb" {
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
-  target_groups = [
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-    }
-  ]
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
   tags = {
     Environment = "dev"
   }
