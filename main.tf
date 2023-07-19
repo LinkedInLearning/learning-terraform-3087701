1bc56d1022cd91cf02c3434403a5e109b3d33d97
DMVTCSTU <42882087+bamchaar@users.noreply.github.com>
2023-07-18 21:16:45 -0400

    fix

--- main.tf
+++ main.tf
@@ -85,9 +85,9 @@ module "alb" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name    = "blog"
 
-  vpc_id = module.blog_vpc.public_subnets[0]
+  name    = "blog"
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
