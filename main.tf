fd2af1b5cd4e623439c77d57122d627b17ee3079
DMVTCSTU <42882087+bamchaar@users.noreply.github.com>
2023-07-18 20:54:28 -0400

    create dev vpc

--- main.tf
+++ main.tf
@@ -17,12 +17,13 @@ data "aws_ami" "app_ami" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "blog-vpc"
+  name = "dev"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
+  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
