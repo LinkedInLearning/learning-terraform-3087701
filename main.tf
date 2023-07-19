03588892ee951dfd8e5e417c7be42d7664f565ae
DMVTCSTU <42882087+bamchaar@users.noreply.github.com>
2023-07-18 20:48:25 -0400

    fixed sg name

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "blog-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
