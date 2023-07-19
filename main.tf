f0b6a4122641d5b13dd68ffe2da802775a9fc332
DMVTCSTU <42882087+bamchaar@users.noreply.github.com>
2023-07-18 21:43:48 -0400

    fixed

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ module "alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.environment.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -72,7 +72,7 @@ module "alb" {
   ]
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
