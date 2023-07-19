9d16921334c6a074e246177bbb7217bbb9284925
DMVTCSTU <42882087+bamchaar@users.noreply.github.com>
2023-07-18 20:19:35 -0400

    use module for security group

--- main.tf
+++ main.tf
@@ -22,11 +22,25 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name    = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
