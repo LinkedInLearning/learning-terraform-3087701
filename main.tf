ff15c3f20d2b880c4927b21a0a2bd69a47fa1100
Mike D <mike.dehart@protonmail.com>
2023-08-28 21:37:45 -0400

    autoscaling

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
@@ -35,8 +35,7 @@ module "alb" {
 
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
-  security_groups    = module.blog_sg.security_group_id
-
+  security_groups    = [module.blog_sg.security_group_id]
   target_groups = [
     {
       name_prefix      = "blog-"
@@ -61,7 +60,7 @@ module "alb" {
   ]
 
   tags = {
-    Environment = "Dev"
+    Environment = "dev"
   }
 }
 
@@ -71,6 +70,7 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -86,7 +86,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog_new"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
