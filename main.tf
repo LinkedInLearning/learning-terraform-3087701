1422a95ccf7fe1a090a9f4142bfcca4d624b711f
Mike D <mike.dehart@protonmail.com>
2023-08-29 19:44:53 -0400

    fixed issue with vpc_id causing SG issues

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "${var.environment.name}-blog-sg"
 
-  vpc_id = module.blog_vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
