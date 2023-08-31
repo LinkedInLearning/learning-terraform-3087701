35e20575256cf0aa15b4612e624b87e8c6131af8
Mohamed Attia <85102022+Attiathedragon@users.noreply.github.com>
2023-09-01 00:35:36 +0300

    test-attia

--- main.tf
+++ main.tf
@@ -18,6 +18,12 @@ data "aws_vpc" "default" {
   default = true
 }
 
+
+data "aws_vpc" "attia" {
+  default = false
+}
+  
+
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
