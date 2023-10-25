33cb5a757b7c8f6999c646f8f7bc2811a8a05c3b
hippoLogic <61562672+hippoLogic@users.noreply.github.com>
2023-10-25 15:43:41 -0500

    Initial commit

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-#variable "instance_type" {
-#  description = "Type of EC2 instance to provision"
-#  default     = "t3.nano"
-#}
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t3.nano"
+}
