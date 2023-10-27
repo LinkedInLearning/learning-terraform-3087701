0a4daac951ca82be4b4d3af1dca510f8d20bf4fc
Luis Mazariegos <39636502+lpmazariegos@users.noreply.github.com>
2023-10-27 17:48:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,6 @@ resource "aws_security_group_rule" "blog_http_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-
 resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
@@ -51,7 +50,6 @@ resource "aws_security_group_rule" "blog_https_in" {
 
   security_group_id = aws_security_group.blog.id
 }
-
 
 resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
@@ -68,8 +66,6 @@ resource "aws_instance" "blog" {
   instance_type          = var.instance_type
   subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog.vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
