71fe178cf0cad9161f083ad88678d403b2c01380
Luis Mazariegos <39636502+lpmazariegos@users.noreply.github.com>
2023-10-27 18:45:52 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "instance_ami" {
-  value = aws_instance.blog.ami
-}
+#output "instance_ami" {
+#  value = aws_instance.blog.ami
+#}
 
-output "instance_arn" {
-  value = aws_instance.blog.arn
-}
+#output "instance_arn" {
+#  value = aws_instance.blog.arn
+#}
