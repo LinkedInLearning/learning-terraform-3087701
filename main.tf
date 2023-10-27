520c4aeb418de2178fa2b701520c56710b1fc886
Luis Mazariegos <39636502+lpmazariegos@users.noreply.github.com>
2023-10-27 18:49:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,17 +30,6 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
