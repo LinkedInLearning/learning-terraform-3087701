a840d9d7e2a9bd4a174afe6da10accb68dbc396e
Luis Mazariegos <39636502+lpmazariegos@users.noreply.github.com>
2023-10-27 17:59:07 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a","us-east-2b","us-east-2c"]
+  azs             = ["us-east-1a","us-east-1b","us-east-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
