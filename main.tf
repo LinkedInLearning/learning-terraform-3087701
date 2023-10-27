c2b8be1edfd25b4fbec824069a205c65c3d3c221
Luis Mazariegos <39636502+lpmazariegos@users.noreply.github.com>
2023-10-27 18:41:15 -0400

    autoscaling

--- main.tf
+++ main.tf
@@ -41,6 +41,22 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
+}
+
+
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
