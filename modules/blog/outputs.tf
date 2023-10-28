aa934ae824691fc389673dc9dcb01af7b6a2e2a2
Luis Mazariegos <39636502+lpmazariegos@users.noreply.github.com>
2023-10-27 23:12:35 -0400

    Update outputs.tf

--- modules/blog/outputs.tf
+++ modules/blog/outputs.tf
@@ -1,11 +1,3 @@
-#output "instance_ami" {
-#  value = aws_instance.blog.ami
-#}
-
-#output "instance_arn" {
-#  value = aws_instance.blog.arn
-#}
-
 output "environment_url" {
     value = module.blog_alb.lb_dns_name
 }
