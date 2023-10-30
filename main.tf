f725287366283c16cd1a7faea7ba7b3dc34559ff
Jack de Kock <55926128+jackdek11@users.noreply.github.com>
2023-10-30 22:40:34 +0200

    fixed azs.public_subnets

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
