4f48410bded2552b4e852a2912013eb0a2f1d996
Jack de Kock <55926128+jackdek11@users.noreply.github.com>
2023-10-30 21:41:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,11 +14,53 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "ChangedName"
   }
 }
+
+resource "aws_security_group" "blog" {
+  name        = "blog"
+  description = "All web traffic ingress. Http / https in and all out"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  cidr_blocks = [0.0.0.0/0]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  cidr_blocks = [0.0.0.0/0]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = [0.0.0.0/0]
+
+  security_group_id = aws_security_group.blog.id
+}
+
