927122126ba7211c39d24cc37508bb1620234901
Jack de Kock <55926128+jackdek11@users.noreply.github.com>
2023-10-30 22:53:27 +0200

    autoscaling

--- main.tf
+++ main.tf
@@ -33,19 +33,24 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "autoscaling" {
+  image_id      = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.1.0"
+  
+  name = "blog"
+  min_size = 1
+  max_size = 2
 
-  tags = {
-    Name = "ChangedName"
-  }
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+
+  security_groups = [module.blog_sg.security_group_id]
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name            = "blog-alb"
