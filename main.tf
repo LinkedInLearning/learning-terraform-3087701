d7ffd38315fe3adaa08e5d1f98f703c9465990d6
Jack de Kock <55926128+jackdek11@users.noreply.github.com>
2023-10-30 22:15:14 +0200

    Use module for security group

--- main.tf
+++ main.tf
@@ -22,18 +22,31 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "ChangedName"
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name    = "blog_new"
+
+  vpc_id  = data.aws_vpc.default.id
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "All web traffic ingress. Http / https in and all out"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id              = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
