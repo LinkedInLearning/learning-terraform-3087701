611aa25231982526516a1dc4cb51471a879ff410
Jack de Kock <55926128+jackdek11@users.noreply.github.com>
2023-10-30 22:29:28 +0200

    fixed bug in aws_instance.blog vpc

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_instance" "blog" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id = module.blog.public_subnets[0]
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "ChangedName"
