3cb3bbb30fa72ee009dd8b77e53b4889a23f4414
Jack de Kock <55926128+jackdek11@users.noreply.github.com>
2023-10-30 22:58:34 +0200

    fixed reference to module.blog_alb.security_group_arn

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arn
+  target_group_arns   = module.blog_alb.security_group_arn
 
   security_groups = [module.blog_sg.security_group_id]
 }
