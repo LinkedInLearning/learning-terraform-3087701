856d4d34c3ff5a0cca1f1f4269c83d24d9d7e57b
Jack de Kock <55926128+jackdek11@users.noreply.github.com>
2023-10-30 21:43:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,6 +40,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
+  protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
@@ -49,6 +50,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
+  protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
