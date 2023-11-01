75dbfaf6b261d7a2ef41bb6c532bc406a23cef77
Vitor Gomes <vitorsgomes@gmail.com>
2023-11-01 18:04:50 +0100

    fix resource name

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_rule" "blog_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
@@ -46,7 +46,7 @@ resource "aws_security_rule" "blog_http_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_rule" "blog_https_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
@@ -56,7 +56,7 @@ resource "aws_security_rule" "blog_https_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_rule" "blog_everything_out" {
+resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
