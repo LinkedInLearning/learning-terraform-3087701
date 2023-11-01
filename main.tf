8bd1d9562f57c15dc033b2a396dd14a037fe5fd7
Vitor Gomes <vitorsgomes@gmail.com>
2023-11-01 18:05:45 +0100

    fix argument

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
-  protocal    = "tcp"
+  protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
@@ -50,7 +50,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
-  protocal    = "tcp"
+  protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
@@ -60,7 +60,7 @@ resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
-  protocal    = "-1"
+  protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
