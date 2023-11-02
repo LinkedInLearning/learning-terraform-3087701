d9e7b41ce25c090ec4e25a8fc37bf5c6853c6c9b
Vitor Gomes <vitorsgomes@gmail.com>
2023-11-02 16:49:45 +0100

    add type and fix security group

--- main.tf
+++ main.tf
@@ -63,11 +63,14 @@ module "alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name    = "blog-alb"
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
 
-  security_groups = module.blog_sg.security_group_id
+  security_groups = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
