81793e30c6ef8716080e8407095e1994bc1058d7
abradley202 <105672783+abradley202@users.noreply.github.com>
2023-11-20 12:47:35 -0800

    updating

--- variables.tf
+++ variables.tf
@@ -2,3 +2,40 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  description = "Name filter and owner for AMI"
+
+  type    = object ({
+    name  = string
+    owner = string
+  })
+
+  default = {
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
+  }
+}
+
+variable "environment" {
+  description = "Deployment environment"
+
+  type        = object ({
+    name           = string
+    network_prefix = string
+  })
+  default = {
+    name           = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "asg_min" {
+  description = "Minimum instance count for the ASG"
+  default     = 1
+}
+
+variable "asg_max" {
+  description = "Maximum instance count for the ASG"
+  default     = 2
+}
