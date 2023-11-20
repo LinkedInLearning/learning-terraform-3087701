3ca2c772cd0d71dd83fd7a7c774564b18a758a44
abradley202 <105672783+abradley202@users.noreply.github.com>
2023-11-20 12:00:54 -0800

    updating

--- main.tf
+++ main.tf
@@ -32,10 +32,10 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = data.aws_vpc.default.id
-  name    = "blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  vpc_id = data.aws_vpc.default.id
+  name                = "blog"
+  ingress_rules       = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
