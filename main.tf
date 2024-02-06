57f178b4ce65fd5e73ae7cb49efb38ae39d7366e
mgreener-2 <96879071+mgreener-2@users.noreply.github.com>
2024-02-06 12:20:40 +0000

    Update main.tf
    Add AWS security group

--- main.tf
+++ main.tf
@@ -14,11 +14,54 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 #AWS Instance Type. 
-resource "aws_instance" "web" {
+resource "aws_instance" "Blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
+}
+
+data "aws_vpc" "default" {
+default = true
+}
+
+resouce "aws_security_group" "blog"
+name        = "blog"
+description = "Allow HTTP and HTTPS in. Allow everything out"
+
+VPC_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_In"{
+type        = "ingress"
+from_port   = 80
+to_port     = 80
+protocol    = "tcp"
+CIDR_Blocks = ["0.0.0.0/0"]
+
+security_group_id =  aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_http_In"{
+type        = "ingress"
+from_port   = 443
+to_port     = 443
+protocol    = "tcp"
+CIDR_Blocks = ["0.0.0.0/0"]
+
+security_group_id =  aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "Blog_everything_out"{
+type        = "egress"
+from_port   = 0
+to_port     = 0
+protocol    = "-1"
+CIDR_Blocks = ["0.0.0.0/0"]
+
+security_group_id =  aws_security_group.blog.id
 }
