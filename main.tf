80154955b566c561ad79b8e7c7b8c267b6cf989c
Shivam Kumar <shivam.sk2003@gmail.com>
2024-02-22 23:23:22 +0530

    vpc.id to vpc_id

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog_new"
 // vpc_id  = module.blog_vpc.vpc_id
-  vpc_id = module.blog_vpc.vpc.id 
+  vpc_id = module.blog_vpc.vpc_id 
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
