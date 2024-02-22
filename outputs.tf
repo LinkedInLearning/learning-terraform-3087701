c40c2fe6bba5011ecbde4dd6ff0be7bcaaee7c3e
Shivam Kumar <shivam.sk2003@gmail.com>
2024-02-22 17:48:37 +0530

    changes vars instance  type

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,6 @@
-#output "instance_ami" {
-#  value = aws_instance.web.ami
-#}
-
-#output "instance_arn" {
-#  value = aws_instance.web.arn
-#}
+output "instance_ami" {
+  value = aws_instance.web.ami
+}
+output "instance_arn" {
+  value = aws_instance.web.arn
+}
