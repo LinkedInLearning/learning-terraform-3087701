6dd0acb854878b60713b6cc58c5053afd0fe8330
Shivam Kumar <shivam.sk2003@gmail.com>
2024-02-22 22:36:22 +0530

    engress to egress

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_security_group_rule" "blog_https_in" {
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
-  type = "engress"
+  type = "egress"
   from_port = 0
   to_port = 0
   protocol = "-1"
