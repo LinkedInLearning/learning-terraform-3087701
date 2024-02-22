469edd201923706bc77c3680c1637966eeda8126
Shivam Kumar <shivam.sk2003@gmail.com>
2024-02-22 17:58:09 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "instance_ami" {
-  value = aws_instance.web.ami
+  value = aws_instance.blog.ami
 }
 output "instance_arn" {
-  value = aws_instance.web.arn
+  value = aws_instance.blog.arn
 }
