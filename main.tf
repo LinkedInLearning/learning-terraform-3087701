020788699d44404cfd4e44285627de203be42fc1
Shivam Kumar <shivam.sk2003@gmail.com>
2024-02-22 23:07:11 +0530

    change version

--- main.tf
+++ main.tf
@@ -29,8 +29,8 @@ resource "aws_instance" "blog" {
   }
 }
 module "blog_sg" {
-  source = "terraform-aws-modules/security_group/aws"
-  version ="4.13.0"
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
   name = "blog_new"
 
   vpc_id = data.aws_vpc.default.id 
