c3bbb9d40551cdf4b0eb5bad37286f42bf251124
Shivam Kumar <shivam.sk2003@gmail.com>
2024-02-22 22:59:33 +0530

    use module for sg

--- main.tf
+++ main.tf
@@ -23,11 +23,25 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
   tags = {
     Name = "HelloWorld"
   }
 }
+module "blog_sg" {
+  source = "terraform-aws-modules/security_group/aws"
+  version ="4.13.0"
+  name = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id 
+
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
 
 resource "aws_security_group" "blog" {
   name = "blog"
@@ -35,6 +49,8 @@ resource "aws_security_group" "blog" {
 
   vpc_id = data.aws_vpc.default.id 
 }
+
+
 
 resource "aws_security_group_rule" "blog_http_in" {
   type = "ingress"
