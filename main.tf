0ea56182c00127648e5c6f702b6167bad05b46ca
Mohammed Saqr <116204515+msaqr27@users.noreply.github.com>
2024-03-15 12:42:52 -0700

    Load Balancer

--- main.tf
+++ main.tf
@@ -25,21 +25,18 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  
-
   tags = {
-    Terraform = "true"
-    Environment = "dev"
+    Terraform    = "true"
+    Environment  = "dev"
   }
 }
 
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.this_security_group_id]
 
   subnet_id = module.blog_vpc.public_subnets[0]
 
@@ -56,11 +53,9 @@ module "blog_sg" {
   description = "Security group for the blog application with HTTP and HTTPS access"
   vpc_id      = module.blog_vpc.vpc_id
 
-  # Define ingress rules for HTTP and HTTPS
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_rules      = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  # Define egress rule to allow all outbound traffic
-  egress_rules = ["all-all"]
+  egress_rules      = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
