4193f21cd703861354c1599caae6f1ccc2d49275
Mohammed Saqr <116204515+msaqr27@users.noreply.github.com>
2024-03-15 12:47:18 -0700

    Load Balancer 2

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-*"]
   }
 
   filter {
@@ -14,34 +14,22 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"]  # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
 module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "<appropriate_version>"
 
-  name = "dev"
+  name = "dev-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
-    Terraform    = "true"
-    Environment  = "dev"
-  }
-}
-
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.this_security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Mohammed_EC2"
+    "Terraform"   = "true"
+    "Environment" = "dev"
   }
 }
 
@@ -49,13 +37,21 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
 
-  name        = "blog_new"
+  name        = "blog-security-group"
   description = "Security group for the blog application with HTTP and HTTPS access"
   vpc_id      = module.blog_vpc.vpc_id
 
-  ingress_rules      = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  egress_rules  = ["all-all"]
+}
 
-  egress_rules      = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [module.blog_sg.this_security_group_id]
+  subnet_id              = element(module.blog_vpc.public_subnets, 0)
+
+  tags = {
+    Name = "Mohammed_EC2"
+  }
 }
