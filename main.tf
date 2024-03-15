a308dd39e99141bee1fe0652b04a422f590ac2bb
Mohammed Saqr <116204515+msaqr27@users.noreply.github.com>
2024-03-15 09:50:00 -0700

    Create New Dev VPC

--- main.tf
+++ main.tf
@@ -14,33 +14,65 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"]  # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
+module "blog_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.2.0"  # Ensure to use a version that works for your needs
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  name = "BlogVPC"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Name = "Mohammed_EC2"
+    "Terraform"    = "true"
+    "Environment"  = "dev"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2"
+  version = "4.0.0"  # Ensure to use a version that works for your needs
 
-  name        = "blog_new"
-  description = "Security group for the blog application with HTTP and HTTPS access"
-  vpc_id      = data.aws_vpc.default.id
+  name        = "blog_sg"
+  description = "Security group for blog application with HTTP and HTTPS access"
+  vpc_id      = module.blog_vpc.vpc_id
 
-  # Define ingress rules for HTTP and HTTPS
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+      description = "Allow HTTP"
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+      description = "Allow HTTPS"
+    }
+  ]
 
-  # Define egress rule to allow all outbound traffic
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+      description = "Allow all outbound traffic"
+    }
+  ]
+}
+
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = "t3.micro"  # Change as per your requirement
+  vpc_security_group_ids = [module.blog_sg.this_security_group_id]
+  subnet_id              = element(module.blog_vpc.public_subnets, 0)
+
+  tags = {
+    "Name" = "Mohammed_EC2"
+  }
 }
